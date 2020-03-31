e1b337ee5a7a7aae4aa3aa858c18b737f38abdc0
Jacob <53273726+Jacob1225@users.noreply.github.com>
2020-03-31 19:00:15 -0400

    Add files via upload

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false; 
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
