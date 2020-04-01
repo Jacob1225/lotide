b637070f6adb8c94024402c629b5a057e96ef645
Jacob <53273726+Jacob1225@users.noreply.github.com>
2020-04-01 15:57:22 -0400

    Add files via upload

---
+++ countLetters.js
@@ -0,0 +1,16 @@
+const countLetters = function(sentence) {
+  const results = {};
+
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+countLetters("lighthouse in the house");
