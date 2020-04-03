5005432aa88ee99d7e0ac59ec4f7073a229070b3
Jacob <53273726+Jacob1225@users.noreply.github.com>
2020-04-02 21:12:38 -0400

    Add files via upload

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  const key = Object.keys(object);
+
+  for (item of key) {
+    if(callback(object[item])) {
+      return item;
+    }
+  }
+};
+
+assertEqual((findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)), 'noma');
