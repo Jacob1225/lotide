31ee93c80814a9d5884f939cac645441ddcd96e7
Jacob <53273726+Jacob1225@users.noreply.github.com>
2020-04-06 13:10:25 -0400

    Add files via upload

--- tail.js
+++ tail.js
@@ -1,19 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   let array2 = array.splice(1,);
   return array2;
 };
+module.exports = tail;
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
 
