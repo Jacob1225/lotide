6952ba8d3fc836490547e98e6ba8999cc5c6c297
Jacob <53273726+Jacob1225@users.noreply.github.com>
2020-04-06 15:14:26 -0400

    Add files via upload

--- middleTest.js
+++ middleTest.js
@@ -1,13 +1,15 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-//TEST CODE
+describe("#middle", () => {
+  it("returns an empty array when passed an array with one element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []);// => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);// => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
--- tailTest.js
+++ tailTest.js
@@ -1,9 +1,20 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+describe("#tail", () => {
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("returns [1, 2, 4, 6] for [1, 1, 2, 4, 6]", () => {
+    assert.deepEqual(tail([1, 1, 2, 4, 6]), [1, 2, 4, 6]);
+  });
+
+  it("returns an empty array if passed an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+});
+
+
 
