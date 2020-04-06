8ade963cb076a00bc9f0d4f230d2611a5116a50e
Jacob <53273726+Jacob1225@users.noreply.github.com>
2020-04-06 16:30:16 -0400

    Add files via upload

--- without.js
+++ without.js
@@ -1,23 +1,6 @@
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`🟢🟢🟢Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-}
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false; 
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 const without = function(source, itemsToRemove) {
   let subset = [];
@@ -29,10 +12,13 @@ const without = function(source, itemsToRemove) {
   return subset;
 };
 
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
 
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+//TEST CODE
+//const words = ["hello", "world", "lighthouse"];
+//console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+//assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+//console.log(without([1, 2, 3], [1])); // => [2, 3]
+//console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
